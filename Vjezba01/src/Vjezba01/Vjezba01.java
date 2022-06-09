21a5e9442e24f9f78abc6c14cfc60ed091f22a3f
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-09 18:16:53 +0200

    Hello world

---
+++ Vjezba01/src/Vjezba01/Vjezba01.java
@@ -0,0 +1,12 @@
+package Vjezba01;
+
+public class Vjezba01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("Ivana Fratric");
+		
+	}
+
+}
---
+++ Vjezba01/src/module-info.java
@@ -0,0 +1,2 @@
+module vjezba01 {
+}
