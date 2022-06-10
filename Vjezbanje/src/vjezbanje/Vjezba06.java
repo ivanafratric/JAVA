d7f2169a060afa66e4576839506aaaf454897d80
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-10 19:31:05 +0200

    Vježbanje

---
+++ Vjezbanje/src/vjezbanje/Vjezba06.java
@@ -0,0 +1,22 @@
+package vjezbanje;
+
+import javax.swing.JOptionPane;
+
+public class Vjezba06 {
+
+	public static void main(String[] args) {
+
+		int prviBroj = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi prvi broj")
+				);
+		
+		int drugiBroj = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi drugi broj")
+				);
+		
+		int rezultat = prviBroj / drugiBroj;
+		
+		System.out.println(rezultat);
+		
+	}
+}
