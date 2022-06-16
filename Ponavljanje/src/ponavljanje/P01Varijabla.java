a1e24c2c5b002e2a0980e078f2426d929809222c
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-16 15:11:04 +0200

    Ponavljanje

---
+++ Ponavljanje/src/ponavljanje/P01Varijabla.java
@@ -0,0 +1,25 @@
+package ponavljanje;
+
+public class P01Varijabla {
+
+	public static void main(String[] args) {
+
+		int i;
+
+		i = 7;
+		
+		System.out.println(i);
+		
+		int t = 3;
+		
+		int x, y, z;
+		
+		int k=3, l=7;
+		
+		int q = l;
+		
+		System.out.println(q);
+
+	}
+
+}
