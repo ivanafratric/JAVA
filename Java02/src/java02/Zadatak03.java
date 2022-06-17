e6b647b61a5afab819729c706cc4c7a4b8ada428
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-17 19:25:08 +0200

    3. zadatak

---
+++ Java02/src/java02/Zadatak03.java
@@ -0,0 +1,22 @@
+package java02;
+
+import javax.swing.JOptionPane;
+
+public class Zadatak03 {
+
+	public static void main(String[] args) {
+
+		int i = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi prvi broj"));
+		
+		int j = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi drugi broj"));
+		
+		if (i>j) {
+			System.out.println(i);
+		} else { System.out.println(j);
+		}
+		
+	}
+
+}
