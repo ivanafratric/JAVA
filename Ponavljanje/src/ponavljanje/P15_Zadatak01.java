ffdf1575a7d4d09d71a00fafce06d3145acff8a3
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-19 17:04:44 +0200

    Ponavljanje

---
+++ Ponavljanje/src/ponavljanje/P15_Zadatak01.java
@@ -0,0 +1,22 @@
+package ponavljanje;
+
+import javax.swing.JOptionPane;
+
+public class P15_Zadatak01 {
+
+	public static void main(String[] args) {
+
+		int i = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi broj"));
+		
+		int j = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi drugi broj"));
+		
+		if (i+j>10) {
+			System.out.println("Osijek");
+		} else {System.out.println("Edunova");
+		}
+		
+	}
+
+}
