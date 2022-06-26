e3cb042e3a923227e0da955b6e160d96d8425af0
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-26 11:56:29 +0200

    Ponavljanje

---
+++ Ponavljanje/src/ponavljanje/P23_ViseDimenzionalniNizovi.java
@@ -0,0 +1,21 @@
+package ponavljanje;
+
+public class P23_ViseDimenzionalniNizovi {
+
+	public static void main(String[] args) {
+
+		int[][] matrica = new int [10][10];
+		
+		matrica[9][9]=7;
+		
+		int[][]tablica= {
+				{1,0,0},
+				{0,1,0},
+				{0,0,1}
+		};
+		
+		System.out.println(tablica);
+		
+	}
+
+}
