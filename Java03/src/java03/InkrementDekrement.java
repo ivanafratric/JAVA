ae728b56b816477f2de4064f25c56b85c480d1f1
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-27 18:09:37 +0200

    Java03

--- Java03/src/java03/InkrementDekrement.java
+++ Java03/src/java03/InkrementDekrement.java
@@ -13,6 +13,16 @@ public class InkrementDekrement {
 		System.out.println(i);
 		System.out.println(--i);
 		
+		
+		int t=2, k=7;
+		
+		t+=++t - k--; //3-7=-4, t=-2 k=6
+		System.out.println("t=" + t + ", k=" + k);
+		
+		k=t++; //k=-2, t=-1
+		System.out.println("t=" + t + ", k=" + k);
+		
+		System.out.println(k+t);
 	}
 
 }
