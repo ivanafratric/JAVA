c1b916045441a3fcfe92abd3db709ea57f27b08f
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-06-28 17:13:06 +0200

    Java04

---
+++ Java04/src/java04/MathKlasa.java
@@ -0,0 +1,21 @@
+package java04;
+
+public class MathKlasa {
+
+	public static void main(String[] args) {
+
+		int i=0;
+		while(true) {
+			i = (int)(Math.random()*100);
+			if(i>=1 && i<=10) {
+				System.out.println(i);
+				break;
+			}
+		}
+		
+		int sb = (int)((Math.random()*(10-1))+1);
+		System.out.println(sb);
+		
+	}
+
+}
