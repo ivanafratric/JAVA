5a8190e4a1b36d48c18dabc0b6cbe8954aefabe0
Ivana Fratrić <104583385+ivanafratric@users.noreply.github.com>
2022-08-10 15:29:23 +0200

    Java04

---
+++ Java04/src/java04/Zadatak01.java
@@ -0,0 +1,23 @@
+package java04;
+
+public class Zadatak01 {
+	
+	public static void main(String[] args) {
+		
+		// Deklarirajte niz decimalnih
+		// brojeva dužine 12 i svim elementima
+		// niza postavite vrijednost 3,14
+		// s while petljom
+		
+		double [] niz = new double [12];
+		int i=0;
+		
+		while(i<niz.length) {
+			niz[i]=3.14;
+			System.out.println(niz[i]);
+			++i;
+		}
+		
+	}
+
+}
